b64e75381de2604bf5561108c71872f38884d646
Shraddha-Choudhary <shraddha.chaudhary000@gmail.com>
2025-11-03 15:16:38 +0530

    shraddha

--- index.js
+++ index.js
@@ -74,6 +74,14 @@ app.use(
   })
 );
 
+// app.use(
+//   cors({
+//     origin: 
+//     "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+
 // ✅ Core middlewares
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true }));
